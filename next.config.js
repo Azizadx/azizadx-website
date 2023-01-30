6d2a9314bb2bd3325cae974d2bfdd80089efeb5a
azizadx <nasrallahh55@gmail.com>
2023-01-30 14:07:57 +0300

    disable image optimazation

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  exportTrailingSlash: true,
+  trailingSlas: true,
+  images: {
+    unoptimized: true
+  },
   exportPathMap: function () {
     return {
       '/': { page: '/' }
